f055b4af4546c991975e692e4502b9d45d22b8c8
Viktor Hegyi <viktorhegyi@gmail.com>
2017-04-06 12:16:49 +0200

    add usage.txt and reading the txt in App.java

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,4 +1,8 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by hegyi on 2017-04-06.
@@ -6,8 +10,19 @@ import java.util.Arrays;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    Path filePath = Paths.get("src/usage.txt");
+    List<String> lines = null ;
+
+    try {
+      lines = Files.readAllLines(filePath);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not read the file!");
+    }
+
+    for (int i = 0; i <lines.size() ; i++) {
+      System.out.println(lines.get(i));
+    }
+
   }
-
-
 }
